ad143dbf1e36be6185a5f1cfc412fb8d52f18251
syrinka <syrinka@foxmail.com>
2023-12-12 21:40:17 +0800

    modded slugcat world state

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
 using BepInEx;
 using Steamworks;
 using ImprovedInput;
@@ -14,7 +17,18 @@ namespace OnlineMapHotkey
     [BepInPlugin("cedaro.online-map-hotkey", "Online Map Hotkey", "1.0")]
     public class Plugin : BaseUnityPlugin
     {
-        protected const string BaseURL = "https://rain-world-map.github.io/map.html";
+        protected static string BaseURL = "https://rain-world-map.github.io/map.html";
+
+        public static Dictionary<string, string> ModdedSlugcats = new Dictionary<string, string>
+        {
+            {"the_similar", "yellow"},
+            {"the_sporecat", "red"},
+            {"the_dronemaster", "artificer"},
+            {"the_outsider", "saint"},
+            {"mirosslug", "spear"},
+        };
+
+        public static List<string> VanillaSlugcats = new List<string> { "white", "yellow", "red", "gourmand", "artificer", "rivulet", "spear", "saint", "sofanthiel" };
 
         public void OnEnable()
         {
@@ -22,12 +36,26 @@ namespace OnlineMapHotkey
             On.Player.checkInput += Player_CheckInput;
         }
 
+        public static string GetWorldState(Player p)
+        {
+            string name = p.slugcatStats.name.value.ToLowerInvariant();
+            if (ModdedSlugcats.TryGetValue(name, out string name2))
+            {
+                return name2;
+            }
+            if (!VanillaSlugcats.Contains(name))
+            {
+                name = "white";
+            }
+            return name;
+        }
+
         public static void Player_CheckInput(On.Player.orig_checkInput orig, Player self)
         {
             if (self.IsKeyBound(Hotkey.Trigger) && Hotkey.Trigger.CheckRawPressed(self.playerState.playerNumber) && self.room.game.IsStorySession)
             {
                 string url = BaseURL
-                    + "?slugcat=" + self.slugcatStats.name.ToString().ToLower()
+                    + "?slugcat=" + GetWorldState(self)
                     + "&region=" + self.room.world.region.name
                     + "&room=" + self.room.roomSettings.name;
                 SteamFriends.ActivateGameOverlayToWebPage(url);
